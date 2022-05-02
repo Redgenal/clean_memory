e9262bfe8150454a6255b71c78c3c7c83aa72b01
Redgenal <50015502+Redgenal@users.noreply.github.com>
2022-05-02 17:32:06 +0300

    v1

---
+++ clean.sh
@@ -0,0 +1,47 @@
+RESET='\033[0m'
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34;1m'
+MAGENTA='\033[35;1m'
+CYAN='\033[36m'
+
+CACHES=~/Library/Caches/*
+
+echo 'BEFORE'
+du -sh ~ 2> errors.txt
+rm -f errors.txt
+rm -rf ~/Library/Caches/*
+rm -rf ~/Library/Application\ Support/Slack/Cache/*
+rm -rf ~/Library/Application\ Support/Code/Crashpad/completed/*
+rm -rf ~/Library/Application\ Support/Code/CachedData/*
+rm -rf ~/Library/Application\ Support/code/User/workspaceStorage/*
+rm -rf ~/Library/42_cach
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/*
+rm -rf ~/Library/Keychains/*
+rm -rf ~/Library/Group\ Containers/*
+echo $GREEN'AFTER'
+du -sh ~ 2> ~errors.txt
+rm -f errors.txt
+echo $YELLOW'\n             *     ,MMM8&&&.            *
+                  MMMM88&&&&&    .
+                 MMMM88&&&&&&&
+     *           MMM88&&&&&&&&
+                 MMM88&&&&&&&&
+                  MMM88&&&&&&
+                    MMM8&&&      *
+          |\___/|
+          )     (             .
+         =\     /=
+           )===(       *
+          /     \
+          |     |
+         /       \
+         \       /
+  _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
+  |  |  |  |( (  |  |  |  |  |  |  |  |  |  |
+  |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |
+  |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |'
